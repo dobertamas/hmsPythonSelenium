a45008295bbab733438f5a9d432e915b542b2666
Tamas Dober <tamas.dober@mobileforming.com>
2018-02-21 15:09:42 -0800

    adding __repr__ method to avoid false messages when debugging

--- hms/pages/login_page.py
+++ hms/pages/login_page.py
@@ -9,11 +9,20 @@ from hms.utilities.file_reader import File_Reader
 class Login_Page(Base_Page):
     log = customLogger(logging.DEBUG)
 
+    """
+    To avoid the following when debugging through PyCharm:
+    Unable to get repr for <class 'hms.pages.login_page.Login_Page'>
+    """
+    def __repr__(self):
+        return "Login_Page"
+
+
     def start(self):
         self.url = '/config/list'
         self.open(self.url)
         self.log.info("Starting the login page ")
         self.assertIn("HMS", self.driver.title)
+
 
     def login(self, username="", password=""):
         file_reader = File_Reader()
@@ -30,14 +39,18 @@ class Login_Page(Base_Page):
             self.log.debug("Login failed ")
             return False
 
+
     def enter_username(self, username):
         self.sendKeys(username, Login_Page_Locators.USERNAME_FIELD, "name")
+
 
     def enterPassword(self, password):
         self.sendKeys(password, Login_Page_Locators.PASSWORD_FIELD, "name")
 
+
     def clickLoginButton(self):
         self.elementClick(Login_Page_Locators.LOGIN_BUTTON, "name")
+
 
     def verify_that_logged_in(self):
         self.assertTrue(self.isElementPresent(Main_Page_Locators.ACTIVE_CLASS, "xpath"),
