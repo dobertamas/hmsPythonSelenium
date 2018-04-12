856fab1e93f783fd4e179bc84103eb1c9926fba5
Tamas Dober <tamas.dober@mobileforming.com>
2018-04-11 20:12:08 -0700

    updating data location

--- hms/utilities/filereader.py
+++ hms/utilities/filereader.py
@@ -1,13 +1,13 @@
 class FileReader():
 
     def get_username(self):
-        my_file = open("test_data.txt", "r")
+        my_file = open("../tests/test_data.txt", "r")
         username = my_file.readline()
         my_file.close()
         return username
 
     def get_password(self):
-        my_file = open("test_data.txt", "r")
+        my_file = open("../tests/test_data.txt", "r")
         my_file.readline()
         password = my_file.readline()
         my_file.close()
